fc0fd9f1b2965aba0478df6f85eb544a99f7b741
Marouan Mbarek <marouan.mbarek@gmail.com>
2026-04-24 20:36:53 +0200

    feat: raspi4b register defines

---
+++ kernel/include/arch/armv8/raspi4b/reg.h
@@ -0,0 +1,27 @@
+#ifndef REG_H
+#define REG_H
+#include <stddef.h>
+#include <stdint.h>
+
+#define DEVICE_START 0xFC000000
+#define DEVICE_BASE 0xFE000000
+#define PERIPHERAL_BASE DEVICE_BASE
+#define UART0_BASE 0xFE201000
+
+inline volatile uint32_t mmio_read(uint32_t reg) {
+  return *(volatile uint32_t *)(reg);
+}
+
+inline void mmio_write(const uint32_t reg, uint32_t val) {
+  *(volatile uint32_t *)(reg) = val;
+}
+
+// Supports only %d, %x, %u, %c, %s
+void kprintf(char *fmt, ...);
+
+void *memset(void *ptr, int c, size_t size);
+
+int fork_process(unsigned long clone_flags, uintptr_t fn, uintptr_t arg,
+                 unsigned long stack);
+
+#endif
