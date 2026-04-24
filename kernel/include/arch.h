e4203b718672b133eaa889dd1cbb7a58611c88ed
Marouan Mbarek <marouan.mbarek@gmail.com>
2026-04-24 20:35:20 +0200

    feat: general arch config header file

---
+++ kernel/include/arch.h
@@ -0,0 +1,32 @@
+
+#ifndef KERNEL_ARCH_H
+#define KERNEL_ARCH_H
+
+/*
+ * ==========
+ * ISA
+ * ==========
+ * Options:
+ *  ARMv8
+ */
+
+#define ISA_ARMv8
+/*
+ * ==========
+ * PLATFORM
+ * ==========
+ *  Options:
+ *    QEMU, BARE
+ */
+#define PLATFORM_QEMU
+
+/*
+ * ==========
+ * BOARD
+ * ==========
+ * Options:
+ *  RASPI4B, VIRT
+ * */
+#define BOARD_RASPI4B
+
+#endif // KERNEL_ARCH_H
