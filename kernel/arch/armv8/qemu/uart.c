35dce5a1c679f6e8ac25ce7560e8732648ec1400
Marouan Mbarek <marouan.mbarek@gmail.com>
2026-04-24 20:41:22 +0200

    fix: include does not exist

--- kernel/arch/armv8/qemu/uart.c
+++ kernel/arch/armv8/qemu/uart.c
@@ -1,7 +1,6 @@
 #include "drivers/uart.h"
 #include "arch/armv8/pl011/pl011_uart.h"
 #include <arch/armv8/qemu/uart.h>
-#include <arch/armv8/raspi4b/io.h>
 #include <stdint.h> // needed for uint32_t type
 
 void uart_init(struct device_uart *uart) { pl011_uart_init(); }
